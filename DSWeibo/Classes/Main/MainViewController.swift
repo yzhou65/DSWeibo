ee6685c2710c47ac7a75d6b01d7707776d59bb9b
barney <barney@gmail.com>
2016-09-06 04:26:14 -0500

    Dynamically get namespace of the project

--- DSWeibo/Classes/Main/MainViewController.swift
+++ DSWeibo/Classes/Main/MainViewController.swift
@@ -23,29 +23,44 @@ class MainViewController: UITabBarController {
         //注意：在ios7以前如果设置了tintColor只有文字会变，而图片不会变
         tabBar.tintColor = UIColor.orangeColor()
         
-        addChildViewController(HomeTableViewController(), title: "首页", imageName: "tabbar_home")
-        addChildViewController(MessageTableViewController(), title: "消息", imageName: "tabbar_message_center")
-        addChildViewController(DiscoverTableViewController(), title: "发现", imageName: "tabbar_discover")
-        addChildViewController(ProfileTableViewController(), title: "我", imageName: "tabbar_profile")
+        addChildViewController("HomeTableViewController", title: "首页", imageName: "tabbar_home")
+        addChildViewController("MessageTableViewController", title: "消息", imageName: "tabbar_message_center")
+        addChildViewController("DiscoverTableViewController", title: "发现", imageName: "tabbar_discover")
+        addChildViewController("ProfileTableViewController", title: "我", imageName: "tabbar_profile")
         
     }
     
     /**
      初始化子控制器。需要传入子控制器对象，标题和图片
      */
-    private func addChildViewController(childController: UIViewController, title:String, imageName:String) {
-        //设置首页tabbar对应的数据
-        childController.tabBarItem.image = UIImage(named: imageName)
-        childController.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
+    private func addChildViewController(childControllerName: String, title:String, imageName:String) {
         
-        //设置导航条对应的数据
-        childController.title = title
+        // <DSWeibo.HomeTableViewController: 0x7ff3a15967c0>
+//        print(childController)
+        
+        //动态获取命名空间
+        let ns = NSBundle.mainBundle().infoDictionary!["CFBundleExecutable"] as! String   //将CFBundleExecutable的value取出来赋给一个String变量
+        
+        //将字符串转换为类（要注意类的命名空间）
+        //默认情况下，命名空间就是项目名称，但是命名空间可以被修改，所以要动态获取命名空间
+        let cls:AnyClass? = NSClassFromString(ns + "." + childControllerName)
+        
+        //通过类创建对象. 要将AnyClass转换为UIViewController的类型
+        let vcCls = cls as! UIViewController.Type
+        let vc = vcCls.init()
+        
+        //设置首页tabbar对应的数据
+        vc.tabBarItem.image = UIImage(named: imageName)
+        vc.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
+        vc.title = title
         
         //给首页包装一个导航控制器
         let nav = UINavigationController()
-        nav.addChildViewController(childController)
+        nav.addChildViewController(vc)
         
         //将导航控制器添加到当前控制器
         addChildViewController(nav)
+        
+        
     }
 }
